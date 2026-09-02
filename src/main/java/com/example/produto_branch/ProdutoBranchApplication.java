33704bd8ba5a5462f3116f985fb37becdab028a3
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-09-02 19:31:18 -0300

    adcionando branch produto

---
+++ src/main/java/com/example/produto_branch/ProdutoBranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.produto_branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoBranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/produto_branch/ProdutoBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.produto_branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
