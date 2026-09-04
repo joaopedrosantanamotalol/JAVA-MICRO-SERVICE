48cd5e3fb6013b850cfa05ac29d4aaa3c6921cac
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-09-04 14:50:36 -0300

    add:gitignore to comercio branch

--- src/main/java/com/example/produto_branch/base/infra/persistence/repository/ProdutoJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.produto_branch.base.infra.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.produto_branch.base.infra.persistence.entities.ProdutoEntity;
-
-public interface ProdutoJpaRepository extends JpaRepository<ProdutoEntity, Long> {
-    
-}
