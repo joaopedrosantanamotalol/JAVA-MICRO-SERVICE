478ab8b450d2d723549f5afaf3d912bcf1bd8cc6
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-09-11 20:23:58 -0300

    a

--- src/main/java/com/usuario/usuario/base/persistence/mapper/UsuarioMapper.java
+++ src/main/java/com/usuario/usuario/base/persistence/mapper/UsuarioMapper.java
@@ -2,6 +2,7 @@ package com.usuario.usuario.base.persistence.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.usuario.usuario.base.application.dto.UsuarioRequest;
 import com.usuario.usuario.base.application.dto.UsuarioResponse;
 import com.usuario.usuario.base.domain.entities.UsuarioEntity;
 import com.usuario.usuario.base.persistence.entities.UsuarioPersistence;
@@ -12,6 +13,8 @@ public interface UsuarioMapper {
     UsuarioPersistence toPersistence(UsuarioEntity entity);
 
     UsuarioEntity toDomain(UsuarioPersistence entity);
+
+    UsuarioEntity toDomain(UsuarioRequest entity);
 
     UsuarioResponse toResponse(UsuarioEntity entity);
 }
