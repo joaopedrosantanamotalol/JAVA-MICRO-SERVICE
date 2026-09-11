80aaf92718b8b48e9c9811cec8c643c6b5b96560
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-09-11 19:55:36 -0300

    add:continue commit

--- src/main/java/com/usuario/usuario/base/application/usecases/criarUsuarioUseCase.java
+++ src/main/java/com/usuario/usuario/base/application/usecases/criarUsuarioUseCase.java
@@ -1,18 +1,28 @@
 package com.usuario.usuario.base.application.usecases;
 
+import com.usuario.usuario.base.application.dto.UsuarioRequest;
+import com.usuario.usuario.base.application.dto.UsuarioResponse;
+import com.usuario.usuario.base.domain.entities.UsuarioEntity;
+import com.usuario.usuario.base.domain.repository.UsuarioRepository;
 import com.usuario.usuario.base.persistence.mapper.UsuarioMapper;
 import com.usuario.usuario.base.persistence.repository.UsuarioJPARepository;
 
 public class criarUsuarioUseCase {
     
     private final UsuarioMapper mapper;
-    private final UsuarioJPARepository repository;
+    private final UsuarioRepository repository;
 
-    public criarUsuarioUseCase(UsuarioMapper mapper, UsuarioJPARepository repository) {
+    
+
+    public criarUsuarioUseCase(UsuarioMapper mapper, UsuarioRepository repository) {
         this.mapper = mapper;
         this.repository = repository;
     }
 
-        
+
+
+    public UsuarioResponse executar(UsuarioRequest request){
+        UsuarioEntity entity = repository.salvar(request);
+    }
 
 }
