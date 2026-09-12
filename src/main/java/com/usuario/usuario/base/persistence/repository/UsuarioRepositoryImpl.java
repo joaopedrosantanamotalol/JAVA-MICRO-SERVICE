887b855f58ce73d71f1100eb88bfacc96d3ac110
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-09-11 21:01:04 -0300

    add: repositoryImpl

---
+++ src/main/java/com/usuario/usuario/base/persistence/repository/UsuarioRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.usuario.usuario.base.persistence.repository;
+
+import com.usuario.usuario.base.domain.repository.UsuarioRepository;
+
+public class UsuarioRepositoryImpl implements UsuarioRepository {
+    
+}
